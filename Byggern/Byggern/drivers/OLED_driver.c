ec541203bd78844714a4cdfd6fe7a8ceb25814b5
Torje Hoås Digernes <torjehoa@pvv.org>
2016-09-21 16:35:11 +0200

    Fleshed out OLED_driver
    Added OLED_draw, which is supposed to copy from memory

--- Byggern/Byggern/drivers/OLED_driver.c
+++ Byggern/Byggern/drivers/OLED_driver.c
@@ -12,27 +12,49 @@ void OLED_command( uint8_t command){
   OLED_command_address=command;
 }
 
-void init_program() {
-  OLED_command(0xae);//Turn off display
-  OLED_command(0xa1);//Map Segment 0-128 in instead of 128-256
-  OLED_command(0xda);//common pads hardware: alternative
-  OLED_command(0x12);
-  OLED_command(0xc8);//common output scan direction:com63~com0
-  OLED_command(0xa8); //multiplex ration mode:63
-  OLED_command(0x3f);
-  OLED_command(0xd5);//display divide ratio/osc. freq. mode
-  OLED_command(0x80);
-  OLED_command(0x81);//contrast control
-  OLED_command(0x50);
-  OLED_command(0xd9);//set pre-charge period
-  OLED_command(0x21);
-  OLED_command(0x20);//Set Memory Addressing Mode
-  OLED_command(0x02);
-  OLED_command(0xdb);//VCOM deselect level mode
-  OLED_command(0x30);
-  OLED_command(0xad);//master configuration
-  OLED_command(0x00);
-  OLED_command(0xa4); //out follows RAM content
-  OLED_command(0xa6); //set normal display
-  OLED_command(0xaf);// display on
+
+#include "../SRAM_utils.h"
+static void * OLED_vram=NULL;
+void OLED_init() {
+  OLED_reset();
+  if ( !OLED_vram){
+    OLED_vram = SRAM_allocate(1024);
+  }else{
+    printf("you already have initialized OLED!\r\n");
+  }
+}
+
+void OLED_reset(){
+
+    OLED_command(0xae);//Turn off display
+    OLED_command(0xa1);//Map Segment 0-128 in instead of 128-256
+    OLED_command(0xda);//common pads hardware: alternative
+    OLED_command(0x12);
+    OLED_command(0xc8);//common output scan direction:com63~com0
+    OLED_command(0xa8); //multiplex ration mode:63
+    OLED_command(0x3f);
+    OLED_command(0xd5);//display divide ratio/osc. freq. mode
+    OLED_command(0x80);
+    OLED_command(0x81);//contrast control
+    OLED_command(0x50);
+    OLED_command(0xd9);//set pre-charge period
+    OLED_command(0x21);
+    OLED_command(0x20);//Set Memory Addressing Mode
+    OLED_command(0x02);
+    OLED_command(0xdb);//VCOM deselect level mode
+    OLED_command(0x30);
+    OLED_command(0xad);//master configuration
+    OLED_command(0x00);
+    OLED_command(0xa4); //out follows RAM content
+    OLED_command(0xa6); //set normal display
+    OLED_command(0xaf);// display on
+}
+
+void OLED_redraw(){
+  for ( int i = 0 ; i < 512;i++){
+    OLED_data_address[i] = OLED_vram[i];
+  }
+  for ( int i = 0 ; i < 512;i++){
+    OLED_data_address[i] = OLED_vram[i+512];
+  }
 }
--- Byggern/Byggern/drivers/OLED_driver.h
+++ Byggern/Byggern/drivers/OLED_driver.h
@@ -3,3 +3,5 @@
 #define OLED_DRIVER_H_
 
 #endif /* OLED_DRIVER_H_ */
+void OLED_init();
+void OLED_reset();
