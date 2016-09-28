8d3acecc2398a4d8c69320f2f5fea56ce3aea333
Space <space@471.no>
2016-09-28 10:19:56 +0200

    Adding initial versions of error handling functions

--- Byggern/Byggern/utils/ERR_utils.c
+++ Byggern/Byggern/utils/ERR_utils.c
@@ -1,1 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define F_CPU 4912000UL
+#include <util/delay.h>
 
+void throwError(error error) {
+	error_not_handled = 1;
+	printf("\n\n [!] Function %s stopped program execution with error code %s. Extra data: %s", error.function, error.code, error.data);
+	while(error_not_handled) {}
+}
--- Byggern/Byggern/utils/ERR_utils.h
+++ Byggern/Byggern/utils/ERR_utils.h
@@ -1,1 +1,7 @@
+typedef struct {
+	char function[10];
+	char code[10];
+	char data[44];
+} error;
 
+void throwError(error error);
