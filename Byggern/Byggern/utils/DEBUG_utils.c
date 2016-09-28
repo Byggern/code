af832d8cffa65b04e353d6370dbe4e4c5debfdac
AudunVN <avn@471.no>
2016-09-28 13:25:43 +0200

    moved printing of ADC values to a debug function in new DEBUG lib

---
+++ Byggern/Byggern/utils/DEBUG_utils.c
@@ -0,0 +1,20 @@
+#include "../macros.h"
+#include "../drivers/UART_driver.h"
+#include "../drivers/EXTMEM_driver.h"
+#include "SRAM_utils.h"
+#include "../drivers/ADC_driver.h"
+#include "../drivers/HID_driver.h"
+#include "../drivers/OLED_driver.h"
+
+void DEBUG_print_HID_values(void) {
+	printf("X\tY\tLS\tRS\tLB\tRB\tDIR\tX\tY\n");
+	
+	const char* dirNames[] = {"L","R","U","D","C"};
+	
+	printf("%3d\t%3d\t", HID_read_joystick_axis(X_AXIS), HID_read_joystick_axis(Y_AXIS));
+	printf("%3d\t%3d\t", HID_read_slider(LEFT_SLIDER), HID_read_slider(RIGHT_SLIDER));
+	printf("%1d\t%1d\t", HID_read_touch_button(LEFT_BUTTON), HID_read_touch_button(RIGHT_BUTTON));
+	printf("%s\t", dirNames[HID_read_joystick_direction()]);
+	JOY_VALS joystickState = HID_read_joystick();
+	printf("%4d\t%4d\r", joystickState.x_axis, joystickState.y_axis);
+}
---
+++ Byggern/Byggern/utils/DEBUG_utils.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef DEBUG_UTILS_H_
+#define DEBUG_UTILS_H_
+
+
+void DEBUG_print_HID_values(void);
+
+
+#endif /* DEBUG_UTILS_H_ */
